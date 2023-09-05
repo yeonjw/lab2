d6a7fa5c99a301b8b9d6297a8fb2e09a5b103c3c
yeonjw <jumpall27@sasa.hs.kr>
2023-09-05 20:02:11 +0900

    Modify count.cc

--- count.cc
+++ count.cc
@@ -1,6 +1,11 @@
 #include "count.h"
+using namespace std;
 
 void count_number() {
     // lab2 //
     // do it yourself //
+	for(int i = 1; i <= 10; i++){
+		cout<<i<<"\n";
+	}
+	cout<<"2019313460\n";
 }
